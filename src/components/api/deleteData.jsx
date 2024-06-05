6a7eb2cdeb45fdf8c86c668437c30823148294e3
Miri_The_Best <miripotter555@gmail.com>
2024-06-05 20:17:07 +0300

    delete mod

--- src/components/api/deleteData.jsx
+++ src/components/api/deleteData.jsx
@@ -4,10 +4,27 @@ import { API_URL } from "./config";; // Assuming you have API_URL defined in you
 const deleteData = async (idOrIdDateString) => {
     try {
         let response;
+        const token = localStorage.getItem('token')
         if (idOrIdDateString.includes('/')) {
-            response = await axios.delete(`${API_URL}/delete_event/` + idOrIdDateString);
+            response = await axios.delete(`${API_URL}/delete_event/` + idOrIdDateString,
+            {
+                headers: {
+                  "Content-Type": "application/json",
+                  Authorization: token,
+                  // Add any other headers here if needed
+                },
+              },
+            );
         } else {
-            response = await axios.delete(`${API_URL}/delete_task/` + idOrIdDateString);
+            response = await axios.delete(`${API_URL}/delete_task/` + idOrIdDateString,
+            {
+                headers: {
+                  "Content-Type": "application/json",
+                  Authorization: token,
+                  // Add any other headers here if needed
+                },
+              },
+            );
         }
         
         // Check response here
